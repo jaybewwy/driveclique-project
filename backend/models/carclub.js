a6e1d410cd5d58dd0d83b3011688c6ed2e234da4
Jay Wells <122325252+jaybewwy@users.noreply.github.com>
2026-05-23 11:36:03 -0700

    `Removed backend code for DriveClique project`

---
+++ backend/models/carclub.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ClubSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  description: { 
+    type: String 
+  },
+  inviteCode: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
+  leader: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  members: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' 
+  }],
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+module.exports = mongoose.model('Club', ClubSchema);
